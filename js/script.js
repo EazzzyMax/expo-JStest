0c0434118ba4f9c27a20d94995b458d919d880d3
seloKaban <maximlegkii@gmail.com>
2021-09-08 18:55:12 +0300

    домой

--- js/script.js
+++ js/script.js
@@ -2,40 +2,41 @@ console.log('хуй');
 document.getElementById('');
 var a = 10;
 console.log(a);
+
+//рандомизатор
 function getRandomInt(min, max) {
    min = Math.ceil(min);
    max = Math.floor(max);
    return Math.floor(Math.random() * (max - min)) + min;
 }
 
-var s = [];
-
-const length = 999;
-
-for (let i = 0; i < length; i++) {
-   s[i] = getRandomInt(1, 1000);
+//random array
+function randomArray(len) {
+   for (let i = 0; i < len; i++) {
+      arr[i] = getRandomInt(1, len);
+   }
+   return arr;
 }
-console.log(s);
-
-var c = 0;
-for (let i = 0; i < length - 1; i++) {
-   for (let j = 0; j < length - 1 - i; j++) {
-      if (s[j] > s[j + 1]) {
-         c = s[j];
-         s[j] = s[j + 1];
-         s[j + 1] = c;
+//bubble sort
+function bubbleSort(arr) {
+   var c = 0;
+   for (let i = 0; i < length - 1; i++) {
+      for (let j = 0; j < length - 1 - i; j++) {
+         if (arr[j] > arr[j + 1]) {
+            c = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = c;
+         }
       }
    }
 }
-console.log(s);
 
-const search = getRandomInt(1, 1000);
 
 function binarSearch(arr) {
    var left = 0;
    var right = arr.length - 1;
    var center = 0;
-   var counter = 0
+   var counter = 0;
    while (left <= right) {
       counter++;
       center = Math.floor((left + right) / 2);
@@ -52,12 +53,16 @@ function binarSearch(arr) {
    return -1;
 }
 
-const ans = binarSearch(s);
-console.log('Искомое: ', search)
-if (ans != -1) {
-   console.log('Ответ: '  ,ans[0]);
-   console.log('Элемент: ' + s[ans[0]]);
-   console.log('Количество иттераций: ' + ans[1])
-} else {
-   console.log('Элемент не найден')
+// const ans = binarSearch(arr);
+// console.log('Искомое: ', search)
+// if (ans != -1) {
+//    console.log('Ответ: '  ,ans[0]);
+//    console.log('Элемент: ' + arr[ans[0]]);
+//    console.log('Количество иттераций: ' + ans[1])
+// } else {
+//    console.log('Элемент не найден')
+// }
+
+function go(form) {
+   
 }
